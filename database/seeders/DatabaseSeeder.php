45e9aaae230788dd9c8d0b4510b56bda6df07585
Omarkhader21 <omarkhaderj21@gmail.com>
2024-09-03 17:52:49 +0300

    Admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         \App\Models\Post::factory(20)->create();
+
+        Admin::create([
+            'name' => 'Admin Name',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('123456789'), // Use a secure password
+        ]);
 
         // User::factory()->create([
         //     'name' => 'Test User',
